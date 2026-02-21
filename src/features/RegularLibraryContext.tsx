284cb872ff7e0bf736d2703f4f90d9dfceeabace
Julia Schiller <3196889+juliaschiller150@users.noreply.github.com>
2026-02-21 08:26:55 -0600

    style: adjust prettier settings a bit to match react-singleton-context

--- src/features/RegularLibraryContext.tsx
+++ src/features/RegularLibraryContext.tsx
@@ -1,11 +1,10 @@
 import * as React from 'react';
 
 export interface IRegularLibraryContext {
-	message: string;
+    message: string;
 }
 
-export const RegularLibraryContext =
-	React.createContext<IRegularLibraryContext | null>(null);
+export const RegularLibraryContext = React.createContext<IRegularLibraryContext | null>(null);
 
 RegularLibraryContext.displayName = 'ExampleLibrary_RegularLibraryContext';
 
