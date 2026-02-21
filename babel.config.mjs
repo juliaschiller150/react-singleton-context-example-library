c5d25aca9be2da6c67c00ce3b3f6d5c4c8eea21f
Julia Schiller <3196889+juliaschiller150@users.noreply.github.com>
2026-02-21 08:45:21 -0600

    style: align more files to new prettier settings

--- babel.config.mjs
+++ babel.config.mjs
@@ -1,8 +1,4 @@
 export default {
-	presets: [
-		'@babel/preset-env',
-		'@babel/preset-react',
-		'@babel/preset-typescript',
-	],
-	plugins: ['@babel/plugin-transform-runtime'],
+    presets: ['@babel/preset-env', '@babel/preset-react', '@babel/preset-typescript'],
+    plugins: ['@babel/plugin-transform-runtime']
 };
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,39 +10,39 @@ import { FlatCompat } from '@eslint/eslintrc';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const compat = new FlatCompat({
-	baseDirectory: __dirname,
-	recommendedConfig: js.configs.recommended,
-	allConfig: js.configs.all,
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all
 });
 
 export default [
-	...compat.extends(
-		'eslint:recommended',
-		'plugin:react/recommended',
-		'plugin:@typescript-eslint/recommended'
-	),
-	{
-		plugins: {
-			react,
-			'@typescript-eslint': typescriptEslint,
-		},
+    ...compat.extends(
+        'eslint:recommended',
+        'plugin:react/recommended',
+        'plugin:@typescript-eslint/recommended'
+    ),
+    {
+        plugins: {
+            react,
+            '@typescript-eslint': typescriptEslint
+        },
 
-		languageOptions: {
-			globals: {
-				...globals.browser,
-			},
+        languageOptions: {
+            globals: {
+                ...globals.browser
+            },
 
-			parser: tsParser,
-			ecmaVersion: 'latest',
-			sourceType: 'module',
-		},
+            parser: tsParser,
+            ecmaVersion: 'latest',
+            sourceType: 'module'
+        },
 
-		settings: {
-			react: {
-				version: 'detect',
-			},
-		},
+        settings: {
+            react: {
+                version: 'detect'
+            }
+        },
 
-		rules: {},
-	},
+        rules: {}
+    }
 ];
